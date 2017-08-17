416216d11cf810e09cccae1d755241459f4b665c
NeonWizard <averagewizard13@gmail.com>
2017-08-17 16:48:09 -0600

    Update CLI to use Curses

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os, sys
+import time
 from twython import Twython, TwythonError, TwythonRateLimitError, TwythonAuthError
+from picker import *
 
 twitterAPI = Twython(os.environ["TWITTER_API_KEY"], os.environ["TWITTER_API_SECRET"], os.environ["TWITTER_TOKEN"], os.environ["TWITTER_TOKEN_SECRET"])
 
@@ -24,13 +26,29 @@ def block(user):
 		print("uh oh")
 		sys.exit()
 
+	time.sleep(1)
 
-for notFollowingID in (followers - following):
-	user = twitterAPI.show_user(user_id=notFollowingID)
 
-	sys.stdout.write("{} ({})".format(user['screen_name'], user['name']))
-	if input().lower() == "n":
-		continue
+# for notFollowingID in (followers - following):
+# 	user = twitterAPI.show_user(user_id=notFollowingID)
 
+# 	sys.stdout.write("{} ({})".format(user['screen_name'], user['name']))
+# 	if input().lower() == "n":
+# 		continue
+
+# 	block(user)
+# 	print()
+
+unwanted = {}
+for ID in followers-following:
+	user = twitterAPI.show_user(user_id=ID)
+	unwanted[("{} (@{})".format(user['name'], user['screen_name']))] = user
+
+chosen = Picker(
+	title = 'Select accounts to block',
+	options = list(unwanted.keys())
+).getSelected()
+
+for key in chosen:
+	user = unwanted[key]
 	block(user)
-	print()

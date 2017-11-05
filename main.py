1732ac93568e8a3201d2ab36dfafbbe88d24a692
maryn <maryn@telepathics.xyz>
2017-11-05 16:59:34 -0700

    comments to remember some different options

--- main.py
+++ main.py
@@ -16,12 +16,18 @@ ENDC = '\033[0m'
 #   -=- CODE TIME -=-
 # ======================
 
-followers = list(twitterAPI.get_followers_ids()['ids'])
+# following = get_friends_ids
+# followers = get_followers_ids
+# unfriend = destroy_friendship
+# block = create_block
+# mute = create_mute
 
-def mute(user):
+following = list(twitterAPI.get_friends_ids()['ids'])
+
+def unfriend(user):
 	try:
-		twitterAPI.create_mute(user_id=user["id"])
-		print(RED + "muted" + ENDC + " {} (@{}).".format(user["name"], user["screen_name"]))
+		twitterAPI.destroy_friendship(user_id=user["id"])
+		print(RED + "unfriendd" + ENDC + " {} (@{}).".format(user["name"], user["screen_name"]))
 	except TwythonRateLimitError:
 		print("uh oh")
 		sys.exit()
@@ -29,28 +35,28 @@ def mute(user):
 	time.sleep(1)
 
 
-# for notFollowingID in (followers - following):
+# for notFollowingID in (following - following):
 # 	user = twitterAPI.show_user(user_id=notFollowingID)
 
 # 	sys.stdout.write("{} ({})".format(user['screen_name'], user['name']))
 # 	if input().lower() == "n":
 # 		continue
 
-# 	mute(user)
+# 	unfriend(user)
 # 	print()
 
 unwanted = {}
-for ID in followers[:70]:
+for ID in following[6:76]:
 	user = twitterAPI.show_user(user_id=ID)
 	unwanted[("{} (@{})".format(user['name'], user['screen_name']))] = user
 
 if len(unwanted) > 0:
 	chosen = Picker(
-		title = 'Select accounts to mute',
+		title = 'Select accounts to unfriend',
 		options = list(unwanted.keys())
 	).getSelected()
 
 	if chosen:
 		for key in chosen:
 			user = unwanted[key]
-			mute(user)
+			unfriend(user)
